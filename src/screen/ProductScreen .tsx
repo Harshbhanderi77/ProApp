eaa6e33e62fdd01df715b7c67f2d7d69a4a4dd5f
Harshbhanderi77 <154979767+Harshbhanderi77@users.noreply.github.com>
2024-06-01 15:41:15 +0530

    init Commit

---
+++ src/screen/ProductScreen .tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export const DetailScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>DetailScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
